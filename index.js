c2c69795caf98a89a4a1d89b0836595764159544
Abi Scholz <Abi.scholz@gmail.com>
2025-03-04 12:17:46 -0500

    removed node modules

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// import and initialize express app
+/* import and initialize express app */
 
 const PORT = 3000;
-// this middleware deals with CORS errors and allows the client on port 5173 to access the server
+/* this middleware deals with CORS errors and allows the client on port 5173 to access the server */
 const cors = require('cors');
-// morgan is a logging library that allows us to see the requests being made to the server
+/* morgan is a logging library that allows us to see the requests being made to the server */
 const morgan = require('morgan');
 
-// set up express middleware
+/* set up express middleware */
 app.use(morgan('dev'));
 const corsOptions = {
   origin: 'http://localhost:5173',
@@ -16,10 +16,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// set up intial hello world route
+/* set up intial hello world route */
 
-// set up api router
+/* set up api router */
 
-// set up error handling
+/* set up error handling (use error handling middleware) */
 
-// initialize server (listen)
+/* our middleware won't capture 404 errors, so we're setting up a separate error handler for those*/
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, can't find that!");
+});
+/* initialize server (listen) */
