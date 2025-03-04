56f1b30306b07f2c0d63e07ad88075032e3eae02
Abi Scholz <Abi.scholz@gmail.com>
2025-03-03 21:48:07 -0500

    Added scaffolding for server-side

---
+++ db/index.js
@@ -0,0 +1,64 @@
+const employees = [
+  {
+    id: 1,
+    name: 'Albert Mills',
+    phone: '(946) 529-4164',
+    isAdmin: false,
+  },
+  {
+    id: 2,
+    name: 'Jose Stamm',
+    phone: '1-405-787-8749 x4311',
+    isAdmin: false,
+  },
+  {
+    id: 3,
+    name: 'Ms. Allison Lehner',
+    phone: '996.680.3262 x467',
+    isAdmin: false,
+  },
+  {
+    id: 4,
+    name: 'Jessie Dibbert',
+    phone: '(204) 816-2524 x53873',
+    isAdmin: false,
+  },
+  {
+    id: 5,
+    name: 'Miss Maria Kiehn',
+    phone: '(206) 990-1990 x8370',
+    isAdmin: false,
+  },
+  {
+    id: 6,
+    name: 'Geraldine King DDS',
+    phone: '337.218.2322 x6035',
+    isAdmin: false,
+  },
+  {
+    id: 7,
+    name: 'Jaime Murazik',
+    phone: '(820) 544-8026 x284',
+    isAdmin: true,
+  },
+  {
+    id: 8,
+    name: 'Brian Monahan',
+    phone: '(839) 289-4790 x338',
+    isAdmin: false,
+  },
+  {
+    id: 9,
+    name: 'Vivian Reinger',
+    phone: '297.696.9011 x342',
+    isAdmin: true,
+  },
+  {
+    id: 10,
+    name: 'Elbert Kiehn',
+    phone: '(339) 543-5497 x24059',
+    isAdmin: false,
+  },
+];
+
+module.exports = employees;
