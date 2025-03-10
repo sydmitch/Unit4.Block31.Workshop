1bc7e5112083d674b62e322becfd4805174e8a6f
Sydney Mitchell <samitchell04@gmail.com>
2025-03-10 13:24:17 -0500

    needing to seed data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* import and initialize express app */
 const express = require('express');
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/acme_hr_db");
 const app = express();
+const PORT = process.env.PORT || a3000;
 
-const PORT = 3000;
 /* this middleware deals with CORS errors and allows the client on port 5173 to access the server */
 const cors = require('cors');
 
@@ -25,19 +27,40 @@ app.get('/', (req, res) => {
 });
 
 /* set up api route */
-app.get('/api/employees', (req, res) => {
-  res.send()
+app.get("/api/employees", async (req, res, next) => {
+  try {
+    const SQL = /*SQL*/ `
+      SELECT * from employees;
+    `
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
 });
 
 /* our middleware won't capture 404 errors, so we're setting up a separate error handler for those*/
 app.use((req, res, next) => {
   res.status(404).send("Sorry, can't find that!");
 });
+
 /* initialize server (listen) */
 const init = async () => {
-  console.log('data seeded');
+  await client.connect();
+  console.log("data seeded");
+  const SQL = /*sql*/ `
+    DROP TABLE IF EXISTS employees;
+    CREATE TABLE employees(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(50),
+      phone VARCHAR(15),
+      isAdmin BOOLEAN DEFAULT false
+    );
+    INSERT INTO employees(name, phone, isAdmin) VALUES('Dummy Data', '14054054055', true);
+  `
+  await client.query(SQL);
   app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+    console.log(`Server is listening on ${PORT}`);
   });
 };
 
